7756bef6289342ca778e58f7cd7bdb67b71edc6e
Amber Taranto <ambzzzz@gmail.com>
2019-09-17 02:19:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,7 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
             t.boolean :family_friendly
             t.datetime :opening_time
             t.datetime :closing_time
-            t.string :description
+            t.text :description
         end
     end
 

c390fcf3c64c028d6ccc64088887ea7796a8dae4
Amber Taranto <ambzzzz@gmail.com>
2019-09-17 02:21:16 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,16 +2,16 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
 
     def change
-        # create_table :haunted_houses do |t|
-        #     t.string :name
-        #     t.string :location
-        #     t.string :theme
-        #     t.float :price
-        #     t.boolean :family_friendly
-        #     t.datetime :opening_time
-        #     t.datetime :closing_time
-        #     t.text :description
-        # end
+        create_table :haunted_houses do |t|
+            t.string :name
+            t.string :location
+            t.string :theme
+            t.float :price
+            t.boolean :family_friendly
+            t.datetime :opening_time
+            t.datetime :closing_time
+            t.text :description
+        end
     end
 
 end
